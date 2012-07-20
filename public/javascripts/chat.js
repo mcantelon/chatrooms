0cc6cfc794c8ff3afd29b227d574b6055f2b55bb
Mike Cantelon <mcantelon@gmail.com>
2012-07-20 00:46:41 -0700

    Got rid of debug line.

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -12,7 +12,6 @@ function processMessage(socket) {
   var message = $('#send-message').val();
 
   if (message[0] == '/') {
-    console.log('command');
     processCommand(socket, message);
   } else {
     sendMessage(socket);
